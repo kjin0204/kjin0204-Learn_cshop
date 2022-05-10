bb517037a3eac22c45d3b5cf3733a967c255ae08
kjin0204 <kin0204@naver.com>
2022-05-10 20:29:13 +0900

    다이스트라 알고리즘 공부

--- Exercise/Program.cs
+++ Exercise/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Exercise
@@ -7,6 +8,17 @@ namespace Exercise
     {
         int[,] adj = new int[6, 6]
          {
+            {-1,15,-1,35,-1,-1 },
+            {15,-1,05,10,-1,-1 },
+            {-1,05,-1,-1,-1,-1 },
+            {35,10,-1,-1,05,-1 },
+            {-1,-1,-1,05,-1,05 },
+            {-1,-1,-1,-1,05,-1 }
+         };
+
+
+        int[,] adj3 = new int[6, 6]
+         {
             {0,1,0,1,0,0 },
             {1,0,1,1,0,0 },
             {0,1,0,0,0,0 },
@@ -14,6 +26,7 @@ namespace Exercise
             {0,0,0,1,0,1 },
             {0,0,0,0,1,0 }
          };
+
 
         List<int>[] adj2 = new List<int>[]
         {
@@ -26,9 +39,127 @@ namespace Exercise
         };
 
 
+        public void Dijikstra(int start)
+        {
+            bool[] visited = new bool[6];
+            int[] distance = new int[6];
+            int[] parent = new int[6];
+            Array.Fill(distance, Int32.MaxValue); //배열의 모든값 변경
+            distance[start] = 0;
+            parent[start] = start;
+
+            while (true)
+            {
+                // 제일 좋은 후보를 찾는다( 가장 가까이에 있는 점)
+
+                //가장 유력한 후보의 거리와 번호를 저장
+                int closest = Int32.MaxValue;
+                int now = -1;
+
+                for(int i =0; i < 6; i++)
+                {
+                    // 이미 방문한 정점은 스킵
+                    if (visited[i])
+                        continue;
+                    // 아직 발견된 적이 없거나 기존후보보다 멀리 있으면 스킵
+                    if (distance[i] == Int32.MaxValue || distance[i] >= closest)
+                        continue;
+                    // 여태껏 발견한 가장 후보라는 의미니까 정보를 갱신
+                    closest = distance[i];
+                    now = i;
+                }
+
+                //다음 후보가 하나도 없다.
+                if (now == -1)
+                    break;
+
+                //제일 좋은 후보를 찾았으니까 방문한다.
+                visited[now] = true;
+                Console.WriteLine(now);
+
+                //방문한 정점과 인접한 정점들을 조사해서 상황에 따라 발견한 최단거리를 갱신
+                for(int next = 0; next< 6; next++)
+                {
+                    //연결되어있지않으면 스킵
+                    if (adj[now, next] == -1)
+                        continue;
+                    //이미 방문한 정점은 스킵
+                    if (visited[next])
+                        continue;
+
+                    //새로 조사된 정점의 최단거리를 계산한다.
+                    int nextDist = distance[now] + adj[now,next];
+
+                    if (distance[next] >= nextDist)
+                    {
+                        distance[next] = nextDist;
+                        parent[next] = now;
+                    }
+                }
+            }
+        }
+
+        bool[] visitt = new bool[6];
+        public void test(int now)
+        {
+            //DPS
+            //Console.WriteLine(now);
+            //visitt[now] = true;
+
+            //for(int i = 0; i < 6; i++)
+            //{
+            //    if (visitt[i])
+            //        continue;
+            //    if (adj3[now, i] != 1)
+            //        continue;
+
+            //    test(i);
+            //}
+
+
+            //BFS
+
+            bool[] found = new bool[6];
+            Queue<int> q = new Queue<int>();
+
+            q.Enqueue(now);
+            int[] distance = new int[6];
+            int[] parent = new int[6];
+
+            distance[now] = 0;
+            distance[now] = now;
+            found[now] = true;
+
+            while (q.Count != 0)
+            {
+                now = q.Dequeue();
+                Console.WriteLine(now);
+
+                for(int next = 0; next < 6; next++)
+                {
+                    if (found[next])
+                        continue;
+                    if (adj3[now, next] == 0)
+                        continue;
+
+                    q.Enqueue(next);
+                    distance[next] = distance[now] + 1;
+                    parent[next] = now;
+                    found[next] = true;
+
+                }
+            }
+
+
+
+
+        }
+
+
         //들어 갔다왔는지 확인하는 변수
         int[] visit = new int[6];
 
+        #region 깊이우선 탐색DPS
         //깊이 우선 탐색1
         public void DPS(int now)
         {
@@ -61,6 +192,7 @@ namespace Exercise
                 DPS2(next);
             }
         }
+        #endregion
 
         //길이 끊겨있을때 노드를 하번씩 다 가기위함.
         public void serchAll()
@@ -75,7 +207,7 @@ namespace Exercise
             }
         }
 
-
+        #region 너비우선 탐색 BFS
         public void BFS(int now)
         {
             int[] found = new int[6];
@@ -132,7 +264,7 @@ namespace Exercise
                 now = que.Dequeue();
                 Console.WriteLine(now);
 
-                foreach(int next in adj2[now])
+                foreach (int next in adj2[now])
                 {
                     if (found[next] == 1)
                         continue;
@@ -147,9 +279,9 @@ namespace Exercise
             }
 
         }
+        #endregion
 
     }
-
 
     class Program
     {
@@ -157,7 +289,7 @@ namespace Exercise
         {
             Console.WriteLine("Hello World!");
             Graph graph = new Graph();
-            graph.BFS2(0);
+            graph.Dijikstra(0);
         }
     }
 }
